987d145833206feea26269f0c63630fd8d8f927a
Kasper Hauschildt <kasperhauschildt@pop-os.localdomain>
2021-01-25 20:16:45 +0100

    recreated the frontpage view from this afternoon

--- db/migrate/20210125144746_create_company_frontpages.rb
+++ db/migrate/20210125144746_create_company_frontpages.rb
@@ -2,7 +2,11 @@ class CreateCompanyFrontpages < ActiveRecord::Migration[6.1]
   def change
     create_table :company_frontpages do |t|
       t.integer :company_id
-
+      t.text :main_image
+      t.string :main_header
+      t.text :about_image
+      t.string :about_header
+      t.text :about_text
       t.timestamps
     end
   end
